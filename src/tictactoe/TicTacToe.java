4554c36939eb0f35d37bdcdfc66250b30191a2d4
USER <USER@192.168.1.108>
2016-05-16 00:09:28 +0400

    TicTacToe by Sultan & Ali

---
+++ src/tictactoe/TicTacToe.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe;
+import javax.swing.JFrame;
+
+
+/**
+ *
+ * @ Sultan & Ali
+ * 
+ */
+public class TicTacToe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame ticTacToe = new Frame();
+        ticTacToe.setTitle("TicTacToe by Sultan & Ali");
+        ticTacToe.setResizable(false);
+        ticTacToe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ticTacToe.setVisible(true);
+    }
+    
+}
